a26c5c20a3089dd01c8785f3ad5fba3aad8c1cfc
Patryk Stefański <119447102+S4ter@users.noreply.github.com>
2023-07-06 12:26:48 +0200

    counting from buttons

---
+++ src/components/feedback/feedback.jsx
@@ -0,0 +1,65 @@
+import React, { Component } from 'react';
+
+export class Feedback extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      good: 0,
+      neutral: 0,
+      bad: 0,
+      total: 0,
+      positiveFeedback: 0,
+    };
+  }
+
+  countPositiveFeedback = () => {
+    const { good, total } = this.state;
+    const positiveFeedback = (good / total) * 100;
+    this.setState({ positiveFeedback });
+  };
+  countTotalFeedback = () => {
+    this.setState({ total: this.state.total + 1 });
+  };
+
+  handleAddGoodReview = () => {
+    console.log('Good was clicked');
+    this.setState({ good: this.state.good + 1 });
+    this.countTotalFeedback();
+    this.countPositiveFeedback();
+  };
+  handleAddNeutralReview = () => {
+    console.log('Neutral was clicked');
+    this.setState({
+      neutral: this.state.neutral + 1,
+    });
+    this.countTotalFeedback();
+  };
+  handleAddBadReview = () => {
+    console.log('Bad was clicked');
+    this.setState({ bad: this.state.bad + 1 });
+    this.countTotalFeedback();
+  };
+
+  render() {
+    const { good, neutral, bad, total, positiveFeedback } = this.state;
+
+    return (
+      <div>
+        <span>Good: {good}</span>
+        <span>Neutral: {neutral}</span>
+        <span>Bad: {bad}</span>
+        <span>Total: {total}</span>
+        <span>Positive Feedback: {positiveFeedback}</span>
+        <button type="button" onClick={this.handleAddGoodReview}>
+          Good {good}
+        </button>
+        <button type="button" onClick={this.handleAddNeutralReview}>
+          Neutral {neutral}
+        </button>
+        <button type="button" onClick={this.handleAddBadReview}>
+          Bad {bad}
+        </button>
+      </div>
+    );
+  }
+}
--- src/index.js
+++ src/index.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { App } from 'components/App';
+import { Feedback } from 'components/feedback/feedback';
 import './index.css';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
+    <Feedback />
     <App />
   </React.StrictMode>
 );
